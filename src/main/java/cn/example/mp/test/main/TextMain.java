23bcedf2ca52b5963af8abbda80a21e0cfd979fc
qxp <1123649805@qq.com>
2020-08-27 10:17:30 +0800

    修改启动端口为10000。TextMain中是一些SpringMVC流程涉及的一些类

--- src/main/java/cn/example/mp/test/main/TextMain.java
+++ src/main/java/cn/example/mp/test/main/TextMain.java
@@ -10,7 +10,13 @@ import com.alibaba.fastjson.JSON;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.util.EntityUtils;
+import org.springframework.messaging.handler.HandlerMethod;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.handler.AbstractHandlerMapping;
+import org.springframework.web.servlet.handler.AbstractHandlerMethodMapping;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import java.io.*;
 import java.net.URISyntaxException;
@@ -22,62 +28,14 @@ import java.util.*;
  **/
 public class TextMain {
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        String uploadUrl = "http://192.168.2.33:8091/api/bsp/fileManager/fileinfo/upload";
-        String downloadUrl = "http://192.168.2.33:8091/api/bsp/fileManager/fileinfo/download";
+    public static void main(String[] args)  {
 
-        //http://192.168.3.238:8090/bbs/Img/02_gettyimages_478845908_high_resized.jpg
-        //17位时间格式 例:202004201139234
-        String replace = DateUtil.formatDate(new Date(), DateUtil.SIMPLE_DATE_PATTERN3)
-                .replace("-", "")
-                .replace(" ", "")
-                .replace(":", "")
-                .replace(".","");
-        //系统流水号
-        String sNo = "S"+"CIM"+replace+UUID.randomUUID().toString().substring(0,5);
-        //全局流水号
-        String gNo = "G"+"CIM"+replace+UUID.randomUUID().toString().substring(0,5);
-        Map<String,String > paraMap = new HashMap<String,String>();
-        paraMap.put("fileName","404.png");
-        paraMap.put("fileFdfsName","wKgChV6EN02Ad-FyAAD5QMTRD3M986.png");
-        paraMap.put("svcRqsSysEnShrtName","CIM");
-        paraMap.put("svcRqsSysTxnTimpstamp",DateUtil.formatDate(new Date(),DateUtil.SIMPLE_DATE_PATTERN3));
-        paraMap.put("svcRqsSysSerNo",sNo);
-        paraMap.put("globalSerialNo",gNo);
-        paraMap.put("origSvcRqsSysEnShrtName","BSP");
-        paraMap.put("origSvcRqsSysTxnTimpstamp",DateUtil.formatDate(new Date(),DateUtil.SIMPLE_DATE_PATTERN3));
-        paraMap.put("flagStatus","");
-        paraMap.put("uploadFileDate","");
-
-        byte []  result = HttpClientUtil.sendGet(downloadUrl, paraMap, "utf-8");
-
-        String bResult = new String(result);
-
-        InputStream inputStream = null;
-        if(bResult.contains("retCode")){
-            System.out.println("下载文件失败");
-        }else{
-            inputStream = new ByteArrayInputStream(result);
-        }
-        OutputStream out = null;
-        File file = new File("E:\\"+"mm3.png");
-        if(!file.exists()){
-            file.createNewFile();
-        }
-        out = new FileOutputStream(file);
-        byte[] buffer= new byte[1024];
-        int len = 0;
-        while ((len=inputStream.read(buffer))!=-1){
-            out.write(buffer,0,len);
-        }
-        if(inputStream!=null){
-            inputStream.close();
-        }
-        if(out!=null){
-            out.close();
-        }
-
-
+        HandlerMethod handlerMethod = null;
+        RequestMappingInfo mappingInfo = null;
+        RequestMappingHandlerMapping requestMappingHandlerMapping = null;
+        DispatcherServlet dispatcherServlet = null;
+        AbstractHandlerMapping mapping = null;
+        AbstractHandlerMethodMapping abstractHandlerMethodMapping = null;
 
 
     }
